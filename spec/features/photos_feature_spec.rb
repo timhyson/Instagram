aebf5c5bb2b522d665a27e4bbd9b5cfe6b2f98f5
Tim Hyson <tjhyson@gmail.com>
2015-10-17 17:35:29 +0100

    photo model created

--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -8,4 +8,17 @@ feature 'photos' do
       expect(page).to have_link 'Add a photo'
     end
   end
+
+  context 'photos have been added' do
+    before do
+      Photo.create(name: 'hello world')
+    end
+
+    scenario 'display photos' do
+      visit '/photos'
+      expect(page).to have_content('hello world')
+      expect(page).not_to have_content('No photos yet')
+    end
+  end
+
 end
