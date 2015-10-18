86518311c4adb34f62478184e8d562e31b02f8ab
Tim Hyson <tjhyson@gmail.com>
2015-10-18 13:40:38 +0100

    user can sign up, in and out

--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -4,12 +4,12 @@ feature 'commenting' do
   before { Photo.create name: 'hello world' }
 
   scenario 'allows users to leave a comment using a form' do
-     visit '/photos'
-     click_link 'Comment on hello world'
-     fill_in 'Thoughts', with: 'Great shot!'
-     click_button 'Leave Comment'
-     expect(current_path).to eq '/photos'
-     expect(page).to have_content('Great shot!')
+    visit '/photos'
+    click_link 'Comment on hello world'
+    fill_in 'Thoughts', with: 'Great shot!'
+    click_button 'Leave Comment'
+    expect(current_path).to eq '/photos'
+    expect(page).to have_content('Great shot!')
   end
 
 end
